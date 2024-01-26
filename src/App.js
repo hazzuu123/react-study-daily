35d61a5bcefd7191b6def5183e2d35ef5c1a818c
하주영 <ju980304@naver.com>
2024-01-26 17:20:26 +0900

    Feat: observer를 활용한 스크롤 애니메이션 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Buttons from "./SVG/Buttons";
 import UserList from "./hooks/useFetch/UserList";
 import SearchBar from "./hooks/useDebouce/SearchBar";
 import UserInfo from "./hooks/useEffect/UserInfo";
+import Observer from "./observer/Observer";
 function App() {
-  return <UserInfo />;
+  return <Observer />;
 }
 
 export default App;
---
+++ src/observer/Observer.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from "react";
+import "./style.css";
+const Observer = () => {
+  // 감시중 박스가 화면에 등장하거나 사라지면 여기 코드를 실행해줌
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // 등장하면 실행
+        entry.target.style.opacity = 1;
+      } else {
+        // 사라지면 실행
+        entry.target.style.opacity = 0;
+      }
+    });
+  });
+
+  // 어떤 요소를 감시할지
+  const divRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+  useEffect(() => {
+    // 옵저버를 각 요소에 연결
+    divRefs.forEach((ref) => {
+      console.log("값:", ref.current);
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+    });
+
+    // 컴포넌트가 언마운트될때 옵저버 정리
+    return () => {
+      divRefs.forEach((ref) => {
+        if (ref.current) {
+          observer.unobserve(ref.current);
+        }
+      });
+    };
+  }, [observer]);
+
+  return (
+    <div>
+      <div ref={divRefs[0]}>
+        <h1>iPhone 15 출시</h1>
+      </div>
+      <div ref={divRefs[1]}>
+        <h1>충전포트를 USB-C 타입으로 바꿔달라고요?</h1>
+      </div>
+      <div ref={divRefs[2]}>
+        <h1>그래서 충전포트를 제거했습니다.</h1>
+      </div>
+      <div ref={divRefs[3]}>
+        <img
+          src="https://cdn.balpumnews.com/news/photo/202306/284_987_4942.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Observer;
