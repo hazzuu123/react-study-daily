a744b37fd96dcfa050dfa593db3142c161d49097
하주영 <ju980304@naver.com>
2024-02-04 12:42:26 +0900

    Refactor: 무한 스크롤 기능 수정
    - useEffect훅에 의존성 배열로 page를 추가하여 page가 변경될때마다 재 실행되도록 설정

--- src/infiniteScroll/InfiniteScroll.js
+++ src/infiniteScroll/InfiniteScroll.js
@@ -3,48 +3,45 @@ import axios from "axios";
 import styles from "./InfiniteScroll.module.css";
 
 const InfiniteScroll = () => {
-  const pageEnd = useRef(null);
+  const pageEndRef = useRef(null); // 페이지 끝을 나타내는 요소에 대한 참조
   const [items, setItems] = useState([]); // 가져올 아이템 리스트
   const [page, setPage] = useState(1); // 페이지 수
 
   useEffect(() => {
+    // IntersectionObserver를 사용하여 페이지 끝에 도달할 때마다 fetchData를 호출
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          setPage((prevPage) => prevPage + 1);
+          fetchData(page);
         }
       },
       { threshold: 1 }
     );
 
-    if (pageEnd.current) {
-      observer.observe(pageEnd.current);
+    if (pageEndRef.current) {
+      observer.observe(pageEndRef.current); // 주시 시작
     }
 
     return () => {
-      if (pageEnd.current) {
-        observer.unobserve(pageEnd.current);
+      if (pageEndRef.current) {
+        observer.unobserve(pageEndRef.current); // 옵저버 제거
       }
     };
-  }, []);
-
-  useEffect(() => {
-    if (page !== 1) {
-      fetchData(page);
-    }
   }, [page]);
 
   /** 데이터 가져오기 */
-  const fetchData = async (pageNumber) => {
+  const fetchData = async (page) => {
     const API_KEY = "_P-zEd16r9d1gojTfckT6Oer2TNspjAxbDmwimgNVQc";
     const response = await axios.get(
-      `https://api.unsplash.com/photos/?client_id=${API_KEY}&page=${pageNumber}&per_page=20`
+      `https://api.unsplash.com/photos/?client_id=${API_KEY}&page=${page}&per_page=20`
     );
     setItems((prevItems) => [...prevItems, ...response.data]);
+    setPage((prev) => prev + 1); // 페이지 증가
   };
 
   return (
     <div>
+      {/* 리스트 */}
       <ul className={styles.container}>
         {items.map((item, index) => (
           <li className={styles.item} key={index}>
@@ -52,7 +49,8 @@ const InfiniteScroll = () => {
           </li>
         ))}
       </ul>
-      <div ref={pageEnd}></div>
+      {/* 리스트 바로 아래를 감지하도록 설정 */}
+      <div ref={pageEndRef}></div>
     </div>
   );
 };
